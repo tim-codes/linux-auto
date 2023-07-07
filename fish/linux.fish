a2968d9f8f649d23fedb353c966399fef2cdaeb7
Tim O'Connell <tim@exxo.sh>
2023-07-07 23:12:51 +0100

    chore(linux.fish): redirect stdout and stderr to /dev/null when setting OpenAI API Key in opencommit config to suppress output

--- fish/linux.fish
+++ fish/linux.fish
@@ -4,4 +4,4 @@ set -x PNPM_HOME $HOME/.local/share/pnpm
 set -x PATH $PATH $PNPM_HOME
 
 # set OpenAI API Key to opencommit config
-opencommit config set OCO_OPENAI_API_KEY=$(cat ~/keys/openai.key)
+opencommit config set OCO_OPENAI_API_KEY=$(cat ~/keys/openai.key) 1&> /dev/null
