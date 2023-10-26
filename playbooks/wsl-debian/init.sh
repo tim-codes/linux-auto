cb961faab9cc39d2d921d6ffd74c37e0221e6ef4
Tim O'Connell <tim@exxo.sh>
2023-10-26 19:32:40 +0100

    feat(init.sh): add installation of wget package to ensure it is available for subsequent commands feat(init.sh): add sourcing of ~/.bashrc after installing nodejs to ensure fnm is available in the current shell session

--- playbooks/wsl-debian/init.sh
+++ playbooks/wsl-debian/init.sh
@@ -1,6 +1,7 @@
 #!/usr/local/env bash
 
 # add repo for just
+sudo apt install -y wget
 wget -qO - 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | sudo tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
 echo "deb [arch=all,$(dpkg --print-architecture) signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | sudo tee /etc/apt/sources.list.d/prebuilt-mpr.list
 sudo apt update
@@ -22,11 +23,11 @@ sudo apt install -y \
   duf \
   bat \
   neofetch \
-  wget \
   git
 
 # install nodejs
 curl -fsSL https://fnm.vercel.app/install | bash
+source ~/.bashrc
 fnm install 18
 fnm default 18
 
