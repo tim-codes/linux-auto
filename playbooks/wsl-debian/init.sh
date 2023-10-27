e6bf9c01cdf0b445d90f1bbb32b94cefc8aa9180
Tim O'Connell <tim@exxo.sh>
2023-10-27 15:09:57 +0100

    feat(init.sh): add installation of git LFS to enable large file support in the repository feat(init.sh): add installation of Node.js using fnm for managing Node.js versions

--- playbooks/wsl-debian/init.sh
+++ playbooks/wsl-debian/init.sh
@@ -25,6 +25,10 @@ sudo apt install -y \
   neofetch \
   git
 
+# install git LFS
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
+sudo apt install -y git-lfs
+
 # install nodejs
 curl -fsSL https://fnm.vercel.app/install | bash
 source ~/.bashrc
