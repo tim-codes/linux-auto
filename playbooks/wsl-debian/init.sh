48327a83870f1382b3df9c86a080176975fe581f
Tim O'Connell <tim@exxo.sh>
2023-11-12 16:42:03 +0000

    feat(init.sh): add installation of Terraform
    The script now includes the installation of Terraform by adding the necessary steps to download and install it. This allows for the usage of Terraform in the WSL Debian environment.

--- playbooks/wsl-debian/init.sh
+++ playbooks/wsl-debian/init.sh
@@ -40,6 +40,17 @@ corepack enable
 corepack prepare pnpm@latest --activate
 pnpm setup
 
+# install terraform
+wget -O- https://apt.releases.hashicorp.com/gpg \
+| gpg --dearmor \
+| sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" \
+| sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install -y terraform
+
 # setup fish shell
 curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source -
 fisher install jorgebucaran/fisher
